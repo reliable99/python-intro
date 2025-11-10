1f82292472c9ba9cd200c1c2f5d3f0a87bde5ed6
reliable99 <abdulmuhminsodiq@gmail.com>
2025-11-10 21:08:07 +0100

    list function

--- list.py
+++ list.py
@@ -1,6 +1,18 @@
 # list is simply a variable where you can store more elements
 # difference between variable and element is that variable contains only one element
-variable1= "apple"
-list_1 =["apple", "banana", "cherry", "melon"]
+# variable1= "apple"
+# list_1 =["apple", "banana", "cherry", "melon"]
+#
+# print(list_1)
 
-print(list_1)
+# using function with list
+
+food =["rice", "pizza", "juice", "fries", "sushi"]
+prices=[5.64,3.92,7.29,4.95,3.20]
+
+food.insert(2,"beans")
+# food.extend(prices)
+food.index("rice")
+# food.clear()
+
+print(food)
